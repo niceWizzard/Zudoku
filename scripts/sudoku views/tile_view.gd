52e9a475539f37f2f2e039252556c1de1dea406c
Richard <richard232manansala@gmail.com>
2024-06-02 22:06:29 +0800

    Added some highlighting features

--- scripts/sudoku views/tile_view.gd
+++ scripts/sudoku views/tile_view.gd
@@ -4,6 +4,12 @@ class_name TileView
 @export var _label : Label
 
 var coordinate : Vector2i
+@onready var highlighted_theme := theme.get_stylebox("highlight", "Tile")
+@onready var normal_theme := theme.get_stylebox("normal", "Tile")
+
+
+func _ready() -> void:
+	theme = theme.duplicate()
 
 func setup(coord : Vector2i) -> void:
 	self.coordinate = coord
@@ -14,3 +20,18 @@ func update_view(val : int) -> void:
 
 func reset() -> void:
 	_label.text = ""
+
+func set_highlighted(is_highlighted: bool) -> void:
+	if is_highlighted:
+		theme.set_stylebox("panel", "Tile", highlighted_theme)
+	else:
+		theme.set_stylebox("panel", "Tile", normal_theme)
+
+
+
+func _on_mouse_exited() -> void:
+	set_highlighted(false)
+
+
+func _on_mouse_entered() -> void:
+	set_highlighted(true)
