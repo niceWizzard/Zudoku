da4c486f2ed2ded4c4f26a06291b596360df0dd3
Richard <richard232manansala@gmail.com>
2024-06-03 15:10:12 +0800

    Made TileView be a button instead of PanelContainer

--- scripts/sudoku views/tile_view.gd
+++ scripts/sudoku views/tile_view.gd
@@ -1,11 +1,7 @@
-extends PanelContainer
+extends Button
 class_name TileView
 
-@export var _label : Label
-
 var coordinate : Vector2i
-@onready var highlighted_theme := theme.get_stylebox("highlight", "Tile")
-@onready var normal_theme := theme.get_stylebox("normal", "Tile")
 
 
 func _ready() -> void:
@@ -16,22 +12,9 @@ func setup(coord : Vector2i) -> void:
 
 
 func update_view(val : int) -> void:
-	_label.text = str(val) if val != 0 else ""
+	text = str(val) if val != 0 else ""
 
 func reset() -> void:
-	_label.text = ""
-
-func set_highlighted(is_highlighted: bool) -> void:
-	if is_highlighted:
-		theme.set_stylebox("panel", "Tile", highlighted_theme)
-	else:
-		theme.set_stylebox("panel", "Tile", normal_theme)
+	text = ""
 
 
-
-func _on_mouse_exited() -> void:
-	set_highlighted(false)
-
-
-func _on_mouse_entered() -> void:
-	set_highlighted(true)
