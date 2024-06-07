94f49d501849363617a8139bcdbee62ff99fcfc5
Richard <richard232manansala@gmail.com>
2024-06-07 20:29:01 +0800

    Removed print statement

--- scripts/views/BoardView.gd
+++ scripts/views/BoardView.gd
@@ -39,7 +39,6 @@ func _ready() -> void:
 			tileViewsMap[Vector2i(x, y)] = tile_view
 		
 func _on_tile_selected(tile : TileView) -> void:
-	prints("TIle View CHange ", tile)
 	for peer : TileView in peerActivatedTileViews:
 			peer.mode_normal()
 	if activeTileView != null:
