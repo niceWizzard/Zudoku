9686653a313ac375fa58d90ba8d5f73a832b3f46
Richard <richard232manansala@gmail.com>
2024-06-11 09:53:35 +0800

    Added scale animations in tile highlight

--- scripts/views/TileView.gd
+++ scripts/views/TileView.gd
@@ -13,6 +13,8 @@ var _is_fixed := false
 
 var stylebox_override := ["normal", "focus", "pressed", "hover"]
 
+var tween : Tween
+
 func fixed() -> void:
 	_is_fixed = true
 
@@ -23,6 +25,9 @@ func setup(coord : Vector2i) -> void:
 	coordinate  =coord
 	gui_input.connect(_on_gui_input)
 	theme = TILE_THEME
+	_on_content_resized()
+	resized.connect(_on_content_resized)
+	
 
 func value() -> int:
 	return int(text)
@@ -48,15 +53,34 @@ func _on_gui_input(event : InputEvent) -> void:
 			tile_selected.emit(self)
 
 func highlight_error() -> void:
+	z_index = 10
 	for override : String in stylebox_override:
 		add_theme_stylebox_override(override, HIGHLIGHT_ERROR_STYLEBOX)
+	_highlight_anim()
 
-
+func _highlight_anim() -> void:
+	if tween != null:
+				tween.kill()
+	tween = create_tween().set_trans(Tween.TRANS_SPRING)
+	tween.tween_property(self, "scale", Vector2.ONE * 1.15, 0.25)
+	tween.tween_property(self, "scale", Vector2.ONE, 0.25)
+	
 func highlight_peer() -> void:
+	z_index = 10
 	for override : String in stylebox_override:
 		add_theme_stylebox_override(override, SELECTED_TILE_STYLEBOX)
+	_highlight_anim()
+	
 
 
 func clear_highlight() -> void:
+	z_index = 0
+	if tween:
+		tween.kill()
+	scale = Vector2.ONE
 	for override : String in stylebox_override:	
 		remove_theme_stylebox_override(override)
+	
+
+func _on_content_resized() -> void:
+	pivot_offset = size / 2.0
